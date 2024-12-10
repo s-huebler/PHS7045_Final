8153b01ab33cfb566bef1a4fe172725d1e06e2b0
Sophie Huebler <sophhuebler@gmail.com>
2024-12-10 12:41:54 -0700

    Github Action Workflow

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,16 @@ library(testthat)
 library(gmhs2)
 
 test_check("gmhs2")
+
+#Checking that an error should be returned if data arguement does
+#not have a column called theta_s
+testthat::expect_error(mhGibbs(data.frame("Trial" = c("A", "B"),
+                                          "rit" = c(15, 17),
+                                          "nit" = c(50, 60),
+                                          "ric" = c(13, 13),
+                                          "nic" = c(50, 55),
+                                          "logOR" = c(0.1, -0.03),
+                                          "gamma_i" = c(0.02, 0.02)),
+                               hyps, mh_scale = 0.2,
+                               sim = 1000, burn = 10,
+                               Bayesian = "hierarchical"))
